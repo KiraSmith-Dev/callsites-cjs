8bb4123b762e341894a9e76e04d6a2873e54bf88
int3nse-git <int3nse-git@users.noreply.github.com>
2022-05-10 16:55:58 -0400

    Increase stack trace limit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = function () {
 	const _prepareStackTrace = Error.prepareStackTrace;
 	Error.prepareStackTrace = (_, stack) => stack;
+	Error.stackTraceLimit = 100;
 	const stack = new Error().stack.slice(1); // eslint-disable-line unicorn/error-message
 	Error.prepareStackTrace = _prepareStackTrace;
 	return stack;
