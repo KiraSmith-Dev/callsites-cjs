8d12deb5ee694fc3774c6ad554eb6f6962356c55
int3nse-git <int3nse-git@users.noreply.github.com>
2022-05-17 03:10:44 -0400

    Restore original stackTraceLimit before returning

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 module.exports = function () {
 	const _prepareStackTrace = Error.prepareStackTrace;
+	const _stackTraceLimit = Error.stackTraceLimit;
+	
 	Error.prepareStackTrace = (_, stack) => stack;
 	Error.stackTraceLimit = 100;
+	
 	const stack = new Error().stack.slice(1); // eslint-disable-line unicorn/error-message
+	
 	Error.prepareStackTrace = _prepareStackTrace;
+	Error.stackTraceLimit = _stackTraceLimit;
+	
 	return stack;
 };
